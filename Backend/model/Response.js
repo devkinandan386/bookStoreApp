3a6e0d5ac0519d5f89ddc94d3a1577910b39e48f
devkinandan386 <158668977+devkinandan386@users.noreply.github.com>
2025-07-31 13:58:16 +0530

    Update Response.js

--- Backend/model/Response.js
+++ Backend/model/Response.js
@@ -3,11 +3,10 @@ import mongoose from "mongoose";
 const responseSchema = mongoose.Schema({
   userId: { type: String, required: true },
   questionId: { type: String, required: true },
-  videoPath: { type: String, required: true },
+  videoPath: { type: String },
   timestamp: { type: Date, default: Date.now },
 });
 
 const Response = mongoose.model("Response", responseSchema);
 
 export default Response;
-
